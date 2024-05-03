127c6250b6a011e9314e009e650c4f3c444f74e3
Alberto Zarzo <stroncio1981@gmail.com>
2024-05-04 01:59:12 +0200

    Challange 6 solved

---
+++ python/reverso.py
@@ -0,0 +1,14 @@
+#
+# Reto #6
+ #INVIRTIENDO CADENAS
+ #Fecha publicación enunciado: 07/02/22
+ #Fecha publicación resolución: 14/02/22
+ #Dificultad: FÁCIL
+#
+# Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+# - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+#
+
+texto = "Hola mundo"
+texto_invertido = texto[::-1]
+print(texto_invertido)
